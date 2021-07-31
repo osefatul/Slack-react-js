b2385933bef48fb8ed26cce5468ea913a595f19a
osefatul <sefaullahomar2@gmail.com>
2021-07-31 08:59:34 -0700

    Header is done

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { Avatar } from "@material-ui/core";
 import AccessTime from "@material-ui/icons/AccessTime";
+import { HelpOutline, Search } from "@material-ui/icons";
 
 function Header() {
   return (
@@ -13,6 +14,17 @@ function Header() {
         />
         <AccessTime />
       </HeaderLeft>
+
+      {/*Header center --- Search bar */}
+      <HeaderSearch>
+        <Search />
+        <input type="text" placeholder="search WDK" />
+      </HeaderSearch>
+
+      {/*Header Right */}
+      <HeaderRight>
+        <HelpOutline />
+      </HeaderRight>
     </HeaderContainer>
   );
 }
@@ -21,6 +33,15 @@ export default Header;
 
 const HeaderContainer = styled.div`
   display: flex;
+  position: fixed;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px 0;
+  background-color: var(
+    --slack-color
+  ); // we defined this variable in the index.css
+  color: white;
 `;
 
 const HeaderLeft = styled.div`
@@ -28,6 +49,50 @@ const HeaderLeft = styled.div`
   display: flex;
   align-items: center;
   margin-left: 20px;
+
+  //lets target component inside the headerLeft whcih is AccessTime Icon
+  > .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 30px;
+  }
 `;
 
-const HeaderAvatar = styled(Avatar)``;
+const HeaderAvatar = styled(Avatar)`
+  cursor: pointer;
+
+  :hover {
+    opacity: 0.8;
+  }
+`;
+
+const HeaderSearch = styled.div`
+  flex: 0.4;
+  opacity: 1;
+  border-radius: 6px;
+  background-color: #421f44;
+  text-align: center;
+  display: flex;
+  padding: 0 50px;
+  color: gray;
+  border: 1px gray solid;
+
+  > input {
+    background-color: transparent;
+    border: none;
+    text-align: center;
+    min-width: 30vw;
+    outline: none;
+    color: white;
+  }
+`;
+
+const HeaderRight = styled.div`
+  flex: 0.3;
+  display: flex;
+  align-items: flex-end;
+
+  > .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 20px;
+  }
+`;
