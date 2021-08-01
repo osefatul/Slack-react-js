abdecb19d03d4d57abbcfc594a347d0c382e5ca1
osefatul <sefaullahomar2@gmail.com>
2021-07-31 18:23:25 -0700

    updated messageInput'

--- src/components/ChatInput.js
+++ src/components/ChatInput.js
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import styled from "styled-components";
 import { Button } from "@material-ui/core";
+import { db } from "./firebase";
+import firebase from "firebase";
 
 function ChatInput({ channelName, channelId }) {
+  const [input, setInput] = useState("");
+
   const sendMessage = (e) => {
     e.preventDefault();
+
+    if (!channelId) {
+      return false; // if there is no channelID just exit
+    }
+
+    db.collection("rooms").doc(channelId).collection("messages").add({
+      message: input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
   };
 
   return (
     <ChatInputContainer>
       <form action="">
-        <input type="text" placeholder={`Message #ROOM`} />
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)} //once text is submitted this will be empty
+          type="text"
+          placeholder={`Message #ROOM`}
+        />
         <Button hidden type="submit" onClick={sendMessage}>
           Send
         </Button>
